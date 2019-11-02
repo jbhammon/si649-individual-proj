722c54bdf862081e1e9067a07a8a596c96377e7c
Jackson Hammond <jbhammon@users.noreply.github.com>
2019-11-02 18:38:44 -0400

    Added all the remaining copy for the algorithms

--- js/myScripts.js
+++ js/myScripts.js
@@ -3,6 +3,13 @@ function switchAlgorithmCopy() {
   $('#bubble-sort-copy').addClass('hidden');
   $('#insertion-sort-copy').addClass('hidden');
   $('#selection-sort-copy').addClass('hidden');
+  $('#odd-even-sort-copy').addClass('hidden');
+  $('#cocktail-sort-copy').addClass('hidden');
+  $('#quicksort-sort-copy').addClass('hidden');
+  $('#heapsort-sort-copy').addClass('hidden');
+  $('#merge-sort-copy').addClass('hidden');
+  $('#bitonic-merge-sort-copy').addClass('hidden');
+  $('#intro-sort-copy').addClass('hidden');
 
   let algorithmName = $('#algo-select').val();
 
@@ -12,6 +19,20 @@ function switchAlgorithmCopy() {
     $('#selection-sort-copy').toggleClass('hidden');
   } else if (algorithmName === 'insertionsort') {
     $('#insertion-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'odd_even_sort') {
+    $('#odd-even-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'cocktail_sort') {
+    $('#cocktail-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'quicksort') {
+    $('#quicksort-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'heapsort') {
+    $('#heapsort-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'mergesort') {
+    $('#merge-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'bitonic_mergesort') {
+    $('#bitonic-merge-sort-copy').toggleClass('hidden');
+  } else if (algorithmName === 'introsort') {
+    $('#intro-sort-copy').toggleClass('hidden');
   } else {
     $('#algorithm-name').text('Nothing!');
   }
